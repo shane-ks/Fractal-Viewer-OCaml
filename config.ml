e12beca1b2e419917c23126e3b1f673979ce8837
shanek-1 <shane.kissinger4@gmail.com>
2023-05-02 17:28:26 -0400

    added write-up

--- config.ml
+++ config.ml
@@ -13,8 +13,8 @@ approximately y = ± 1.12, respectively.
 open ComplexNum ;; 
 
 (* sets the width and height of the window *)
-let width = 800 ;;
-let height = 600 ;; 
+let width = 1600 ;;
+let height = 1200 ;; 
 (* sets the minimum and maximum real value to compute *)
 let xmin = -.2.05 ;;
 let xmax = 0.6 ;;
@@ -24,7 +24,7 @@ let ymax = 1.14 ;;
 let color = true ;; 
 (* sets the max number of iterations of the fractal equation. Increasing the 
    max number of iterations makes it slower but gives a better picture. *)
-let max_step = 25;; 
+let max_step = 200;; 
 (* sets the threshold to stop iterating. Keep as default of 2.1 for z^2 + c*)
 let threshold = 2.1 ;;
 (* define the fractal equation below *)
